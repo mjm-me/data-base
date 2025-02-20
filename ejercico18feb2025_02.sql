6f6f28905b51fa5ce2f9056f6fa4e7b39f408c8a
mj <mjmartinsu@yahoo.es>
2025-02-20 10:06:40 +0100

    Initial commit

--- ejercico18feb2025_02.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM products;
-
-SELECT ProductID, ProductName, SupplierName, CategoryName
- from products p 
- join suppliers s on p.SupplierID = s.SupplierID
- join categories c on p.CategoryID = c.CategoryID;
